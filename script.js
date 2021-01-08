ece31dffcffdbe27b7b88c8d72ab2c965f52136b
thomas <thomas.c.arcos@gmail.com>
2021-01-08 15:46:57 -0300

    card rotations

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const secondsClass = document.querySelector('.seconds');
 const minutesClass = document.querySelector('.minutes');
 const hoursClass = document.querySelector('.hours');
 const daysClass = document.querySelector('.days');
+const secondsCard = document.querySelector('.box-seconds');
+const minutesCard = document.querySelector('.box-minutes');
+const hoursCard=document.querySelector('.box-hours');
+const daysCard = document.querySelector('box-days');
 
 
 const getRemainTime = deadline =>{
@@ -23,14 +27,31 @@ const getRemainTime = deadline =>{
     }
 } 
 
-
 function updateCountdown(){
+    let rotationSeconds =0,rotationMinutes=0,rotationHours=0,rotationDays=0;
     let date = new Date("2021/12/24 24:00:00");
     setInterval(() => {
+    
     secondsClass.innerHTML = getRemainTime(date).remainSeconds;
     minutesClass.innerHTML=getRemainTime(date).remainMinutes;
     hoursClass.innerHTML=getRemainTime(date).remainHours;
     daysClass.innerHTML=getRemainTime(date).remainDays;
+
+    //cards rotation
+    rotationSeconds +=360;
+    secondsCard.style.transform=`rotatex(${rotationSeconds}deg)`;
+    if(secondsClass.innerHTML=="00"){
+        rotationMinutes+=360;
+        minutesCard.style.transform=`rotatex(${rotationMinutes}deg)`;
+    }
+    if(minutesClass.innerHTML=="00"){
+        rotationHours+=360;
+        hoursCard.style.transform=`rotatex(${rotationHours}deg)`;
+    }
+    if(hoursClass.innerHTML=="00"){
+        rotationDays+=360;
+        daysCard.style.transform=`rotatex(${rotationDays}deg)`;
+    }
     }, 1000);
 }
 updateCountdown();
@@ -41,3 +62,5 @@ setInterval(() => {
     move +=1;
     document.querySelector('.container').style.backgroundPosition=`${move}px`;
 }, 30);
+
+
