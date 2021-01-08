3365105fe6885f3f64c20e9d46ac38f1cbfbf280
thomas <thomas.c.arcos@gmail.com>
2021-01-08 01:15:37 -0300

    background movement,new date,new resolutions

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const minutesClass = document.querySelector('.minutes');
 const hoursClass = document.querySelector('.hours');
 const daysClass = document.querySelector('.days');
 
-let date = new Date("2020/12/24 24:00:00");
+
 const getRemainTime = deadline =>{
     let now = new Date(),
     remainTime =(new Date(deadline) - now + 1000) /1000;
     remainSeconds = ('0'+Math.floor(remainTime % 60)).slice(-2),
     remainMinutes = ('0'+Math.floor(remainTime / 60 % 60)).slice(-2),
     remainHours = ('0'+Math.floor(remainTime / 3600 % 24)).slice(-2),
-    remainDays = ('0'+Math.floor(remainTime / (3600 * 24))).slice(-2)
+    remainDays = ('0'+Math.floor(remainTime / (3600 * 24))).slice(-3)
 
     return {
         remainTime,
@@ -25,6 +25,7 @@ const getRemainTime = deadline =>{
 
 
 function updateCountdown(){
+    let date = new Date("2021/12/24 24:00:00");
     setInterval(() => {
     secondsClass.innerHTML = getRemainTime(date).remainSeconds;
     minutesClass.innerHTML=getRemainTime(date).remainMinutes;
@@ -33,3 +34,10 @@ function updateCountdown(){
     }, 1000);
 }
 updateCountdown();
+
+//background movement
+var move = 1;
+setInterval(() => {
+    move +=1;
+    document.querySelector('.container').style.backgroundPosition=`${move}px`;
+}, 30);
